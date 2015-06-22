1fbe2f1c3e1c87cfa5611cd2d2043813555bf76f
mattdesl <dave.des@gmail.com>
2015-06-22 16:11:15 -0400

    streetview stuff

---
+++ examples/streetview.js
@@ -0,0 +1,80 @@
+/*globals google*/
+var THREE = global.THREE = require('three')
+var equirect = require('google-panorama-equirectangular')
+var panorama = require('google-panorama-by-location')
+var createOrbitViewer = require('three-orbit-viewer')(THREE)
+var streetview = require('awesome-streetview')
+
+var InputTexture = require('../nodes/InputEquirectangularTexture')
+var SuperMultisampleBlur = require('../nodes/SuperMultisampleBlur')
+
+var app = createOrbitViewer({
+  clearColor: 0xffffff,
+  clearAlpha: 1.0,
+  fov: 45,
+  near: 0.0001,
+  position: new THREE.Vector3(0, 0, -0.1)
+})
+
+var texture = new THREE.Texture()
+texture.minFilter = THREE.LinearFilter
+texture.generateMipmap = false
+
+// transparent canvas to start (white)
+var canvas = document.createElement('canvas')
+texture.needsUpdate = true
+texture.image = canvas
+
+// add a double-sided sphere
+var cubeTex = new THREE.CubeTexture()
+var geo = new THREE.SphereGeometry(1, 84, 84)
+var mat = new THREE.MeshBasicMaterial({
+  side: THREE.DoubleSide,
+})
+var sphere = new THREE.Mesh(geo, mat)
+
+var itemGeo = new THREE.TorusKnotGeometry(0.02, 0.008, 128, 128)
+var itemMat = new THREE.MeshPhongMaterial({
+  transparent: true,
+  shininess: 20,
+  metal: true,
+  color: 0xffffff,
+  combine: THREE.BlendOperation,
+  reflectivity: 0.95
+})
+var item = new THREE.Mesh(itemGeo, itemMat)
+
+var light = new THREE.PointLight( 0xffffff, 1, 500 )
+light.position.set( 0.025, 0.1, -0.025 )
+app.scene.add( light )
+
+var ambient = new THREE.AmbientLight( 0x404040 )
+app.scene.add(ambient)
+app.scene.add(sphere)
+sphere.material.map = texture
+
+// load a random panosphere / streetview
+var location = streetview()
+console.log(location)
+panorama(location, function (err, result) {
+  if (err) throw err
+  // load the equirectangular image
+  equirect(result.id, {
+    tiles: result.tiles,
+    canvas: canvas,
+    crossOrigin: 'Anonymous',
+    zoom: 4
+  })
+    .on('progress', function () {
+      texture.needsUpdate = true
+    })
+    .on('complete', function (image) {
+      texture.needsUpdate = true
+      var renderer = app.renderer
+      var input = new InputTexture(renderer, texture)
+      var blur = new SuperMultisampleBlur(renderer, input, 0.2, 2)
+      input.update()
+      item.material.envMap = blur.texture
+      app.scene.add(item)
+    })
+})
---
+++ nodes/InputEquirectangularTexture.js
@@ -0,0 +1,37 @@
+var Signal = require('signals').Signal;
+var config = require('../config');
+
+function CubeMapNodeEquirectangularTexture (renderer, texture) {
+  this.renderer = renderer;
+  this.update = this.update.bind(this);
+
+  var scene = new THREE.Scene();
+  var camera = new THREE.CubeCamera(0.1, 1000, 256, config.textureType);
+  scene.add(camera);
+  var updateSignal = new Signal();
+
+  var sphereGeometry = new THREE.SphereGeometry(1, 32, 16);
+  var sphereMaterial = new THREE.MeshBasicMaterial({
+    map: texture,
+    side: THREE.DoubleSide
+  });
+  var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  scene.add(sphere)
+
+  this._sphereMaterial = sphereMaterial
+  this.updateSignal = updateSignal;
+  this.camera = camera;
+  this.scene = scene;
+  this.texture = camera.renderTarget;
+}
+
+CubeMapNodeEquirectangularTexture.prototype.setTexture = function(texture) {
+  this._sphereMaterial.map = texture
+}
+
+CubeMapNodeEquirectangularTexture.prototype.update = function() {
+  this.camera.updateCubeMap(this.renderer, this.scene);
+  this.updateSignal.dispatch();
+}
+
+module.exports = CubeMapNodeEquirectangularTexture ;
